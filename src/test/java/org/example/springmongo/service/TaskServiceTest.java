cba8e98b6a94c5b6731fbf9f06619fcd24b9e48c
jardelrodriguespinto <ghmagno910@gmail.com>
2024-07-04 21:04:07 -0300

    Adicionando testes unitários na aplicação

---
+++ src/test/java/org/example/springmongo/service/TaskServiceTest.java
@@ -0,0 +1,39 @@
+package org.example.springmongo.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskServiceTest {
+
+
+    @Test
+    void taskRepository()
+    {
+    }
+
+    @Test
+    void findTaskById() {
+    }
+
+    @Test
+    void findAllTasks() {
+    }
+
+    @Test
+    void addTask() {
+    }
+
+    @Test
+    void updateTask() {
+    }
+
+    @Test
+    void deleteTaskById() {
+    }
+
+    @Test
+    void deleteTaskByTitleAndDescription() {
+    }
+}
